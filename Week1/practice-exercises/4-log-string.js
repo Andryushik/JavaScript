a310057b9208ef78c1b13f05a989b8a55d62040b
Andrei <68118535+Andryushik@users.noreply.github.com>
2022-10-24 15:57:15 +0200

    Update 4-log-string.js

--- Week1/practice-exercises/4-log-string.js
+++ Week1/practice-exercises/4-log-string.js
@@ -4,20 +4,20 @@
  */
 
 // 1. Declare a variable myString and assign a string to it. Use your full name, including spaces, as the content for the string.
-
+let myString = 'Andrei Popov';
 
 // 2. Write a console.log statement in which you explain in words what you think the value of the string is.
-
+console.log('String value is my full name');
 
 // 3. Now console.log the variable myString.
-
+console.log(myString);
 
 // 4. Now reassign to the variable myString a new string.
-
+myString = 'Andrei';
 
 // 5. Just like what you did before write a console.log statement that explains in words what you think will be logged to the console.
-
+console.log('String value is my name');
 
 // 6. Now console.log myString again.
-
+console.log(myString);
 

8ba7550a6d2f91347bb7795ea8b55d8f474b447a
Andrei <68118535+Andryushik@users.noreply.github.com>
2022-10-24 15:45:37 +0200

    Update 3-log-number.js

--- Week1/practice-exercises/3-log-number.js
+++ Week1/practice-exercises/3-log-number.js
@@ -4,20 +4,22 @@
  */
 
 // 1. Declare your variable numberX. Do not initialize it (which means, don't give it a starting value) yet
-
+let numberX;
 
 // 2. Add a console.log statement that explains in words what you think the value of x is
-
+console.log('X number is not defined');
 
 // 3. Add a console.log statement that logs the value of numberX.
-
+console.log(numberX);
 
 // 4. Now initialize your variable numberX with a number (also called an integer in computer science terms)
-
+numberX = 5;
 
 // 5. Next, add a console.log statement that explains what you think the value of numberX is
-
+console.log('X number is now 5');
 
 // 6. Add a console.log statement that logs the value of numberX
+
+console.log(numberX);
 
 

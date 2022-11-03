03cb773047db6bbc9a038c41bf58e7c49f477b1f
Andrei <68118535+Andryushik@users.noreply.github.com>
2022-11-03 19:57:49 +0100

    Update traffic-light.js

--- Week3/prep-exercises/1-traffic-light/traffic-light.js
+++ Week3/prep-exercises/1-traffic-light/traffic-light.js
@@ -1,52 +1,49 @@
-"use strict";
-/**
- * The `trafficLight` object is now no longer a global variable. Instead,
- * it is defined in function `main()` and passed as a parameter to other
- * functions, as and when needed.
- */
+'use strict';
 
 function getCurrentState(trafficLight) {
-  // TODO
-  // Should return the current state (i.e. colour) of the `trafficLight`
-  // object passed as a parameter.
+  return trafficLight.possibleStates[trafficLight.stateIndex];
 }
 
 function getNextStateIndex(trafficLight) {
-  // TODO
-  // Return the index of the next state of the `trafficLight` such that:
-  // - if the color is green, it will turn to orange
-  // - if the color is orange, it will turn to red
-  // - if the color is red, it will turn to green
+
+//   if (trafficLight.stateIndex === 0) {
+//     return (trafficLight.stateIndex = 1);
+//   } else if (trafficLight.stateIndex === 1) {
+//     return (trafficLight.stateIndex = 2);
+//   } else {
+//     return (trafficLight.stateIndex = 0);
+//   }
+  
+// The same but shorter with ternary operators
+  return trafficLight.stateIndex === 0
+    ? (trafficLight.stateIndex = 1)
+    : trafficLight.stateIndex === 1
+    ? (trafficLight.stateIndex = 2)
+    : (trafficLight.stateIndex = 0);
 }
 
-// This function loops for the number of seconds specified by the `secs`
-// parameter and then returns.
-// IMPORTANT: This is not the recommended way to implement 'waiting' in
-// JavaScript. You will learn better ways of doing this when you learn about
-// asynchronous code.
 function waitSync(secs) {
   const start = Date.now();
-  while (Date.now() - start < secs * 1000) {
-    // nothing do to here
-  }
+  while (Date.now() - start < secs * 1000) {}
 }
 
 function main() {
   const trafficLight = {
-    possibleStates: ["green", "orange", "red"],
+    possibleStates: ['green', 'orange', 'red'],
     stateIndex: 0,
   };
 
   for (let cycle = 0; cycle < 6; cycle++) {
     const currentState = getCurrentState(trafficLight);
-    console.log(cycle, "The traffic light is now", currentState);
-
-    waitSync(1); // Wait a second before going to the next state
+    console.log(cycle, 'The traffic light is now', currentState);
+    waitSync(1);
     trafficLight.stateIndex = getNextStateIndex(trafficLight);
+    console.log(getNextStateIndex(trafficLight));
   }
 }
 
 main();
+
 /**
  * The output should be:
 
